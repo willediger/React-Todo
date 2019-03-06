9545a7e94044f17e3b78be468c985e159e4948ee
will Ediger <will.ediger@gmail.com>
2019-03-05 20:29:23 -0600

    added missing input to TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 const TodoForm = (props) => (
   <>
     <form>
+      <input placeholder="...todo" />
       <button>add</button>
       <button>clear selected</button>
     </form>
